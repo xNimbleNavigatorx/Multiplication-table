ee9257e165c82b640a553345f598b0a7c824d1dc
xNimbleNavigatorx <alexandrehankel@hanovernorwichschools.org>
2016-10-24 13:50:01 -0400

    Updated from Brython Server: 10/24/2016 1:50:01 PM

--- multiplication-table.py
+++ multiplication-table.py
@@ -21,3 +21,13 @@ Height of multiplication table: 8
   7  14  21  28  35  42  49  56  63  70
   8  16  24  32  40  48  56  64  72  80
 """
+
+
+print([x*1 for x in range(1,11)])
+print([x*2 for x in range(1,11)])
+print([x*3 for x in range(1,11)])
+print([x*4 for x in range(1,11)])
+print([x*5 for x in range(1,11)])
+print([x*6 for x in range(1,11)])
+print([x*7 for x in range(1,11)])
+print([x*8 for x in range(1,11)])
